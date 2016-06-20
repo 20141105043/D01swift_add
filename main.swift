a77005ca0258f40342bf4a0d104523f268f74279
20141105043 <670140530@qq.com>
2016-06-20 10:53:54 +0800

    add from 1 to 99
    first swift project,Xcode 7.3,OS X application

---
+++ main.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  01_swift_d
+//
+//  Created by 20141105043dj on 16/6/20.
+//  Copyright © 2016年 20141105043dj. All rights reserved.
+//
+
+import Foundation
+
+var i=0
+
+var sum=0
+
+while i < 100 {
+    i=i+1
+    sum=sum+i
+}
+
+print(sum)
+
